101e49dbd2826f78dd50aa1467a00d6c5a3e162f
RogovoyAlexandr <rogovoyalexandr94@gmail.com>
2018-10-14 21:46:16 +0300

    Publish/Subscribe

---
+++ main/publish_subscribe.js
@@ -0,0 +1,57 @@
+(function () {
+    var _pubsub = {},
+        topics = [],
+        uid = -1;
+
+
+    _pubsub.publish = function (event, args) {
+        if (!topics[event])
+            return false;
+
+        setTimeout(function () {
+            var subscribers = topics[event],
+                len = subscribers ? subscribers.length : 0;
+
+            while (len--)
+                subscribers.func(event, args)
+        });
+
+        return true;
+    };
+
+    _pubsub.subscribe = function (event, func) {
+        if (!topics[event])
+            topics[event] = [];
+
+        var token = ++uid;
+
+        topics[event].push({
+            token: token,
+            func: func
+        });
+
+        return token;
+    };
+
+    _pubsub.unsubscribe = function (event, token) {
+        if (!topics[event])
+            return false;
+
+        for (var i = 0; i < topics[event].length; ++i) {
+            if (topics[event][i].token === token) {
+                topics[event].splice(i, 1);
+                return token;
+            }
+        }
+
+        return false;
+
+    };
+
+    function getPubSub() {
+        return _pubsub;
+    }
+
+    window.pubsub = getPubSub();
+
+})();
